cd2219d442453022cae32094c08223b9b8fc60d7
Dina Muravska <dina.muravska.25@gmail.com>
2026-06-12 20:11:36 +0300

    add userSchema

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,13 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+  {
+    username: { type: String, trim: true },
+    email: { type: String, unique: true, required: true, trim: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true },
+);
 
 // Перевизначаємо метод toJSON
 userSchema.methods.toJSON = function () {
@@ -9,4 +16,10 @@ userSchema.methods.toJSON = function () {
   return obj;
 };
 
-export const User = model("user", userSchema);
+userSchema.pre("save", function () {
+  if (!this.username) {
+    this.username = this.email;
+  }
+});
+
+export const User = model("User", userSchema);
