95dbcd2dc93eb83771b74fd746e4072d7dc09001
Dina Muravska <dina.muravska.25@gmail.com>
2026-06-12 20:26:23 +0300

    add loginUserSchema

--- src/models/user.js
+++ src/models/user.js
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 const userSchema = new Schema(
   {
-    username: { type: String, trim: true },
+    name: { type: String, trim: true, required: true },
     email: { type: String, unique: true, required: true, trim: true },
     password: { type: String, required: true },
   },
@@ -15,11 +15,5 @@ userSchema.methods.toJSON = function () {
   delete obj.password;
   return obj;
 };
-
-userSchema.pre("save", function () {
-  if (!this.username) {
-    this.username = this.email;
-  }
-});
 
 export const User = model("User", userSchema);
