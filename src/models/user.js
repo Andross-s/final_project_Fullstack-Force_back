8e8ac59e7165ee16d23930f69d2a33b727a62f71
IhorKM <11kozenkoihor@gmail.com>
2026-06-12 19:01:39 +0200

    feat: update user and recipe schemas

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,21 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    avatar: { type: String, default: null },
+    refreshToken: { type: String, default: null },
+    favorites: [{ type: Schema.Types.ObjectId, ref: "recipe" }],
+  },
+  { timestamps: true },
+);
 
-// Перевизначаємо метод toJSON
 userSchema.methods.toJSON = function () {
   const obj = this.toObject();
   delete obj.password;
+  delete obj.refreshToken;
   return obj;
 };
 
