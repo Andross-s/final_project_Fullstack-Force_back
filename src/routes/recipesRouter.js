896602357b03e8395e5052406cc6fbf6d6a90563
Andrej Uljanov <anulian22@gmail.com>
2026-06-12 17:34:42 +0200

    Add logout endpoint and favorites endpoint

--- src/routes/recipesRouter.js
+++ src/routes/recipesRouter.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { getFavorites } from "../controllers/recipes/getFavorites.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const recipesRouter = Router();
+
+
+recipesRouter.get("/favorites", authMiddleware, getFavorites);
 
 export default recipesRouter;
