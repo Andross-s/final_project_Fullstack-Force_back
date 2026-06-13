bfdedcbaa9ec3398dd66e4afe921687b74c3713b
Dina Muravska <dina.muravska.25@gmail.com>
2026-06-13 18:26:41 +0300

    add fix

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,14 +2,11 @@ import { Router } from "express";
 import { celebrate } from "celebrate";
 import { loginUserSchema } from "../validations/authValidation.js";
 import { auth } from "../controllers/index.js";
-import { registerUserSchema } from "../validations/authValidation.js";
 import { registerUser } from "../controllers/auth/registerUser.js";
-
-
 
 const authRouter = Router();
 
-authRouter.post("/register",celebrate(registerUserSchema) ,registerUser);
+authRouter.post("/register", auth.registerUser);
 
 authRouter.post("/login", celebrate(loginUserSchema), auth.loginUser);
 
