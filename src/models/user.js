83f7dbe05ba57b9bb8be59133a72994edc1abf69
Ruslan Kirvas <kirvas.ruslan@gmail.com>
2026-06-13 10:19:55 +0200

    feat: complete registration with session support

--- src/models/user.js
+++ src/models/user.js
@@ -1,19 +1,43 @@
-import { Schema, model } from "mongoose";
+
+
+import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
   {
-    name: { type: String, trim: true, required: true },
-    email: { type: String, unique: true, required: true, trim: true },
-    password: { type: String, required: true },
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String,
+      default: "https://ac.goit.global/fullstack/react/default-avatar.jpg"
+    }
   },
-  { timestamps: true },
+  {
+    timestamps: true
+  }
 );
+userSchema.pre("save", function () {
+if (!this.username)
+this.username = this.email;
+});
 
-// Перевизначаємо метод toJSON
 userSchema.methods.toJSON = function () {
   const obj = this.toObject();
   delete obj.password;
   return obj;
 };
 
-export const User = model("User", userSchema);
+export const User = mongoose.model("User", userSchema); 
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,10 +2,14 @@ import { Router } from "express";
 import { celebrate } from "celebrate";
 import { loginUserSchema } from "../validations/authValidation.js";
 import { auth } from "../controllers/index.js";
+import { registerUserSchema } from "../validations/authValidation.js";
+import { registerUser } from "../controllers/auth/registerUser.js";
+
+
 
 const authRouter = Router();
 
-authRouter.post("/register", auth.registerUser);
+authRouter.post("/register",celebrate(registerUserSchema) ,registerUser);
 
 authRouter.post("/login", celebrate(loginUserSchema), auth.loginUser);
 
