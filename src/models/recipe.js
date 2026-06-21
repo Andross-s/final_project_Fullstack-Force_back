3dfe7f6052d2104d13a3c333aac4ceb3251a51ac
Andrij Skoropad <andross_s@ukr.net>
2026-06-21 10:45:48 +0300

    Revert "Improve recipe model, filters and similar recipes logic"

--- src/models/recipe.js
+++ src/models/recipe.js
@@ -2,39 +2,23 @@ import { Schema, model } from "mongoose";
 
 const recipeSchema = new Schema(
   {
-    // 📝 Назва рецепта
     title: { type: String, required: true },
-
-    // 🧾 Опис
     description: { type: String },
-
-    // 📖 Інструкція приготування
     instructions: { type: String },
-
-    // 🖼 Фото рецепта
     thumb: {
       type: String,
+      required: null,
       default:
         "https://res.cloudinary.com/dkiruwtcx/image/upload/q_auto/f_auto/v1781512091/Photo_dkn9mn.png",
     },
-
-    // ⏱ Час приготування
     time: { type: Number },
-
-    // 🔥 Калорії
     calories: { type: Number, default: null },
-
-    // 🏷 Категорія (ВИПРАВЛЕНО: було categories)
-    category: {
+    categories: {
       type: Schema.Types.ObjectId,
       ref: "category",
       required: true,
     },
-
-    // 👤 Власник рецепта
     owner: { type: Schema.Types.ObjectId, ref: "user", default: null },
-
-    // 🧂 Інгредієнти
     ingredients: [
       {
         ingredient: { type: Schema.Types.ObjectId, ref: "ingredient" },
